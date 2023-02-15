74510a43d697ebdc650eff827938043dbeacfd69
hmtimany <hmtimany@gmail.com>
2023-02-15 13:24:58 +0200

    fix utils

--- src/Glb.Common/GlbServices.cs/SMS_Service.cs
+++ src/Glb.Common/GlbServices.cs/SMS_Service.cs
@@ -211,33 +211,4 @@ namespace Glb.Common.GlbServices
             }
         }
     }
-
-    public partial class Extensions
-    {
-        #region  "MobileNumber"
-        public static string? ToShortMobileNumber(this string MobileNumber, string Separator = "", string[]? MobileCodes = null)
-        {
-            if (MobileCodes == null)
-                MobileCodes = new string[] { "3", "70", "71", "76", "78", "79", "81" };
-            string mobilenumber = MobileNumber;
-            mobilenumber = System.Text.RegularExpressions.Regex.Replace(MobileNumber, "[^0-9]", "");
-            if (mobilenumber.Length < 7)
-                return null;
-            string BaseNumber = mobilenumber.Substring(mobilenumber.Length - 6);
-            mobilenumber = mobilenumber.Replace(BaseNumber, "");
-            string? Code = MobileCodes.ToList().Where(_code => mobilenumber.EndsWith((int.Parse(_code)).ToString())).FirstOrDefault();
-            if (string.IsNullOrWhiteSpace(Code))
-                return null;
-            mobilenumber = string.Format("{0}{1}{2}", Code.PadLeft(2, '0'), Separator, BaseNumber);
-            return mobilenumber;
-        }
-        public static string? ToLongMobileNumber(this string MobileNumber, string[]? MobileCodes = null)
-        {
-            string? shortmobilenumber = ToShortMobileNumber(MobileNumber, MobileCodes: MobileCodes);
-            if (!string.IsNullOrWhiteSpace(shortmobilenumber))
-                return "+961" + int.Parse(shortmobilenumber).ToString();
-            return null;
-        }
-        #endregion
-    }
 }
