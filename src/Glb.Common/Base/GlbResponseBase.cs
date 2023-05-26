7a5c489c643d60a8eaf92c4107c88512ffaa8d23
mmtairek1 <mazen.mtairek@idm.net.lb>
2023-05-26 16:02:20 +0300

    Updated the redirect functionality

--- src/Glb.Common/Base/GlbResponseBase.cs
+++ src/Glb.Common/Base/GlbResponseBase.cs
@@ -1,9 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace Glb.Common.Base;
 public class GlbResponseBase
 {
     public int Status { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public object? Data { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? Message { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? RedirectAction { get; set; }
 
     public GlbResponseBase()
